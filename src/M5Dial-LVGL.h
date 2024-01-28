91b8c0374a5bfe0853c4ca521b96938c4141542f
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-01-28 17:04:03 +0900

    Add lvgl display init part

---
+++ src/M5Dial-LVGL.h
@@ -0,0 +1,14 @@
+#ifndef M5DIAL_LVGL_H
+#define M5DIAL_LVGL_H
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+void lv_port_disp_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
--- src/lv_port_disp.cpp
+++ src/lv_port_disp.cpp
@@ -4,26 +4,20 @@
  */
 
 /*Copy this file as "lv_port_disp.c" and set this value to "1" to enable content*/
-#if 0
+#if 1
 
 /*********************
  *      INCLUDES
  *********************/
-#include "lv_port_disp_template.h"
+#include "lv_port_disp.h"
 #include <stdbool.h>
+#include <M5Unified.hpp>
 
 /*********************
  *      DEFINES
  *********************/
-#ifndef MY_DISP_HOR_RES
-    #warning Please define or replace the macro MY_DISP_HOR_RES with the actual screen width, default value 320 is used for now.
-    #define MY_DISP_HOR_RES    320
-#endif
-
-#ifndef MY_DISP_VER_RES
-    #warning Please define or replace the macro MY_DISP_HOR_RES with the actual screen height, default value 240 is used for now.
-    #define MY_DISP_VER_RES    240
-#endif
+#define MY_DISP_HOR_RES    240
+#define MY_DISP_VER_RES    240
 
 /**********************
  *      TYPEDEFS
@@ -66,19 +60,19 @@ void lv_port_disp_init(void)
     static lv_color_t buf_1_1[MY_DISP_HOR_RES * 10];                          /*A buffer for 10 rows*/
     lv_display_set_buffers(disp, buf_1_1, NULL, sizeof(buf_1_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    /* Example 2
-     * Two buffers for partial rendering
-     * In flush_cb DMA or similar hardware should be used to update the display in the background.*/
-    static lv_color_t buf_2_1[MY_DISP_HOR_RES * 10];
-    static lv_color_t buf_2_2[MY_DISP_HOR_RES * 10];
-    lv_display_set_buffers(disp, buf_2_1, buf_2_2, sizeof(buf_2_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    // /* Example 2
+    //  * Two buffers for partial rendering
+    //  * In flush_cb DMA or similar hardware should be used to update the display in the background.*/
+    // static lv_color_t buf_2_1[MY_DISP_HOR_RES * 10];
+    // static lv_color_t buf_2_2[MY_DISP_HOR_RES * 10];
+    // lv_display_set_buffers(disp, buf_2_1, buf_2_2, sizeof(buf_2_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    /* Example 3
-     * Two buffers screen sized buffer for double buffering.
-     * Both LV_DISPLAY_RENDER_MODE_DIRECT and LV_DISPLAY_RENDER_MODE_FULL works, see their comments*/
-    static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];
-    static lv_color_t buf_3_2[MY_DISP_HOR_RES * MY_DISP_VER_RES];
-    lv_display_set_buffers(disp, buf_3_1, buf_3_2, sizeof(buf_3_1), LV_DISPLAY_RENDER_MODE_DIRECT);
+    // /* Example 3
+    //  * Two buffers screen sized buffer for double buffering.
+    //  * Both LV_DISPLAY_RENDER_MODE_DIRECT and LV_DISPLAY_RENDER_MODE_FULL works, see their comments*/
+    // static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];
+    // static lv_color_t buf_3_2[MY_DISP_HOR_RES * MY_DISP_VER_RES];
+    // lv_display_set_buffers(disp, buf_3_1, buf_3_2, sizeof(buf_3_1), LV_DISPLAY_RENDER_MODE_DIRECT);
 
 }
 
@@ -115,17 +109,10 @@ void disp_disable_update(void)
 static void disp_flush(lv_display_t * disp_drv, const lv_area_t * area, uint8_t * px_map)
 {
     if(disp_flush_enabled) {
-        /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
-
-        int32_t x;
-        int32_t y;
-        for(y = area->y1; y <= area->y2; y++) {
-            for(x = area->x1; x <= area->x2; x++) {
-                /*Put a pixel to the display. For example:*/
-                /*put_px(x, y, *px_map)*/
-                px_map++;
-            }
-        }
+        int32_t width = area->x2 - area->x1 + 1;
+        int32_t height = area->y2 - area->y1 + 1;
+        M5.Display.setAddrWindow(area->x1, area->y1, width, height);
+        M5.Display.pushPixels((uint16_t*)px_map, width * height, true);
     }
 
     /*IMPORTANT!!!
